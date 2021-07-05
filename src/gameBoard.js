3dadbd167c7a492def342d16b4b55d80191f5c2e
Sebastian Drewes <sebas.drewes@gmail.com>
2021-07-05 18:13:36 -0300

    pensada logica placeship

--- src/gameBoard.js
+++ src/gameBoard.js
@@ -1,15 +1,36 @@
+/* eslint-disable indent */
+import myModule from './shipFactory';
+
+const { shipFactory } = myModule;
+
 const GameBoard = () => {
   const gameBoard = ['', '', '', '', '', '', '', '', '', '',
-    '', '', '', '', '', '', '', '', '', '',
-    '', '', '', '', '', '', '', '', '', '',
-    '', '', '', '', '', '', '', '', '', '',
-    '', '', '', '', '', '', '', '', '', '',
-    '', '', '', '', '', '', '', '', '', '',
-    '', '', '', '', '', '', '', '', '', '',
-    '', '', '', '', '', '', '', '', '', '',
-    '', '', '', '', '', '', '', '', '', '',
-    '', '', '', '', '', '', '', '', '', ''];
-  return gameBoard;
+                     '', '', '', '', '', '', '', '', '', '',
+                     '', '', '', '', '', '', '', '', '', '',
+                     '', '', '', '', '', '', '', '', '', '',
+                     '', '', '', '', '', '', '', '', '', '',
+                     '', '', '', '', '', '', '', '', '', '',
+                     '', '', '', '', '', '', '', '', '', '',
+                     '', '', '', '', '', '', '', '', '', '',
+                     '', '', '', '', '', '', '', '', '', '',
+                     '', '', '', '', '', '', '', '', '', ''];
+
+  const placeShipHorizontal = (index, length) => {
+    const newShip = shipFactory(length);
+    for (let i = 0; i < newShip.hitPoints.length; i += 1) {
+      gameBoard[index + i] = 'ship';
+    }
+};
+    const placeShipVertical = (index, length) => {
+        const newShip = shipFactory(length);
+        for (let i = 0; i < newShip.hitPoints.length * 10; i += 10) {
+          gameBoard[index + i] = 'ship';
+        }
+    };
+
+  return {
+    placeShipHorizontal, placeShipVertical, gameBoard,
+  };
 };
 
 export default {
--- src/gameBoard.test.js
+++ src/gameBoard.test.js
@@ -3,5 +3,19 @@ import myModule from './gameBoard';
 const { GameBoard } = myModule;
 
 test('test', () => {
-  expect(GameBoard().length).toBe(100);
+  const newBoard = GameBoard();
+  newBoard.placeShipHorizontal(0, 5);
+  newBoard.placeShipVertical(5, 6);
+
+  expect(newBoard.gameBoard[0]).toBe('ship');
+  expect(newBoard.gameBoard[1]).toBe('ship');
+  expect(newBoard.gameBoard[2]).toBe('ship');
+  expect(newBoard.gameBoard[3]).toBe('ship');
+  expect(newBoard.gameBoard[4]).toBe('ship');
+  expect(newBoard.gameBoard[5]).toBe('ship');
+  expect(newBoard.gameBoard[15]).toBe('ship');
+  expect(newBoard.gameBoard[25]).toBe('ship');
+  expect(newBoard.gameBoard[35]).toBe('ship');
+  expect(newBoard.gameBoard[45]).toBe('ship');
+  expect(newBoard.gameBoard[55]).toBe('ship');
 });
