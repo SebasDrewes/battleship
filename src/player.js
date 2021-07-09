223b903102e4d88a01623690c3614e1c3bcb217a
Sebastian Drewes <sebas.drewes@gmail.com>
2021-07-09 12:09:25 -0300

    arreglado movimiento valido jugador

--- src/player.js
+++ src/player.js
@@ -1,15 +1,20 @@
 const player = ((playerGameBoard, enemyGameBoard) => {
   const takeTurn = (index) => {
-    enemyGameBoard.receiveAttack(index);
-    const validArray = [];
-    for (let i = 0; i < playerGameBoard.gameBoard.length; i += 1) {
-      if (playerGameBoard.gameBoard[i] !== 'hit' && playerGameBoard.gameBoard[i] !== 'noHit') {
-        validArray.push(i);
-      }
-    }
-    const randomIndex = validArray[Math.floor(Math.random() * validArray.length)];
+    // solo ejecuta funcion si selecciona un movimiento valido.
+    if (enemyGameBoard.gameBoard[index] !== 'hit' && enemyGameBoard.gameBoard[index] !== 'noHit') {
+      enemyGameBoard.receiveAttack(index);
 
-    playerGameBoard.receiveAttack(randomIndex);
+      const validArray = [];
+      // loop para pushear index vacios para luego seleccionar uno random;
+      for (let i = 0; i < playerGameBoard.gameBoard.length; i += 1) {
+        if (playerGameBoard.gameBoard[i] !== 'hit' && playerGameBoard.gameBoard[i] !== 'noHit') {
+          validArray.push(i);
+        }
+      }
+      const randomIndex = validArray[Math.floor(Math.random() * validArray.length)];
+
+      playerGameBoard.receiveAttack(randomIndex);
+    }
   };
 
   return {
