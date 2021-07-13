312b4ba289a80a71938d18a67dabac2ee94ec55c
Sebastian Drewes <sebas.drewes@gmail.com>
2021-07-13 19:52:42 -0300

    agregado segundo ciclo de inteligencia IA

--- src/player.js
+++ src/player.js
@@ -1,4 +1,6 @@
+/* eslint-disable no-restricted-globals */
 let lastIndex = '';
+let anteultimoIndex = '';
 const playerTurn = (playerGameBoard, enemyGameBoard, index) => {
   function compareElements(arr1, item) {
     return arr1.some((number) => item === number);
@@ -19,19 +21,21 @@ const playerTurn = (playerGameBoard, enemyGameBoard, index) => {
     // IA para probar casilleros adjacentes si ultimo moviemiento fue hit
     if (playerGameBoard.gameBoard[lastIndex] === 'hit') {
       validArray = [];
-      if (playerGameBoard.gameBoard[lastIndex - 1] === '' || !(isNaN(playerGameBoard.gameBoard[lastIndex - 1]))
+      anteultimoIndex = lastIndex;
+      if ((playerGameBoard.gameBoard[lastIndex - 1] === '' || !(isNaN(playerGameBoard.gameBoard[lastIndex - 1])))
       && compareElements(invalidIndexArray, playerGameBoard.gameBoard[lastIndex - 1]) === false) {
         validArray.push(lastIndex - 1);
       }
-      if (playerGameBoard.gameBoard[lastIndex + 1] === '' || !(isNaN(playerGameBoard.gameBoard[lastIndex + 1]))
+      if ((playerGameBoard.gameBoard[lastIndex + 1] === '' || !(isNaN(playerGameBoard.gameBoard[lastIndex + 1])))
       && compareElements(invalidIndexArray, playerGameBoard.gameBoard[lastIndex + 1]) === false) {
         validArray.push(lastIndex + 1);
       }
-      if (playerGameBoard.gameBoard[lastIndex + 11] === '' || !(isNaN(playerGameBoard.gameBoard[lastIndex + 11]))
+
+      if ((playerGameBoard.gameBoard[lastIndex + 11] === '' || !(isNaN(playerGameBoard.gameBoard[lastIndex + 11])))
       && compareElements(invalidIndexArray, playerGameBoard.gameBoard[lastIndex + 11]) === false) {
         validArray.push(lastIndex + 11);
       }
-      if (playerGameBoard.gameBoard[lastIndex - 11] === '' || !(isNaN(playerGameBoard.gameBoard[lastIndex - 11]))
+      if ((playerGameBoard.gameBoard[lastIndex - 11] === '' || !(isNaN(playerGameBoard.gameBoard[lastIndex - 11])))
       && compareElements(invalidIndexArray, playerGameBoard.gameBoard[lastIndex - 11]) === false) {
         validArray.push(lastIndex - 11);
       }
@@ -43,11 +47,38 @@ const playerTurn = (playerGameBoard, enemyGameBoard, index) => {
           }
         }
       }
-      console.log(lastIndex);
-      console.log(validArray);
-      console.log(playerGameBoard.gameBoard);
-    }
+    } else if (playerGameBoard.gameBoard[anteultimoIndex] === 'hit' && playerGameBoard.gameBoard[lastIndex] === 'noHit') {
+      validArray = [];
+      if ((playerGameBoard.gameBoard[anteultimoIndex - 1] === '' || !(isNaN(playerGameBoard.gameBoard[anteultimoIndex - 1])))
+        && compareElements(invalidIndexArray,
+          playerGameBoard.gameBoard[anteultimoIndex - 1]) === false) {
+        validArray.push(anteultimoIndex - 1);
+      }
+      if ((playerGameBoard.gameBoard[anteultimoIndex + 1] === '' || !(isNaN(playerGameBoard.gameBoard[anteultimoIndex + 1])))
+        && compareElements(invalidIndexArray,
+          playerGameBoard.gameBoard[anteultimoIndex + 1]) === false) {
+        validArray.push(anteultimoIndex + 1);
+      }
 
+      if ((playerGameBoard.gameBoard[anteultimoIndex + 11] === '' || !(isNaN(playerGameBoard.gameBoard[anteultimoIndex + 11])))
+        && compareElements(invalidIndexArray,
+          playerGameBoard.gameBoard[lastIndex + 11]) === false) {
+        validArray.push(anteultimoIndex + 11);
+      }
+      if ((playerGameBoard.gameBoard[anteultimoIndex - 11] === '' || !(isNaN(playerGameBoard.gameBoard[anteultimoIndex - 11])))
+        && compareElements(invalidIndexArray,
+          playerGameBoard.gameBoard[anteultimoIndex - 11]) === false) {
+        validArray.push(anteultimoIndex - 11);
+      }
+      if (!validArray[0]) {
+        for (let i = 0; i < playerGameBoard.gameBoard.length; i += 1) {
+          if (playerGameBoard.gameBoard[i] !== 'hit' && playerGameBoard.gameBoard[i] !== 'noHit'
+              && compareElements(invalidIndexArray, playerGameBoard.gameBoard[i]) === false) {
+            validArray.push(i);
+          }
+        }
+      }
+    }
     // seleccion un index valido
     const randomIndex = validArray[Math.floor(Math.random() * validArray.length)];
     lastIndex = randomIndex;
